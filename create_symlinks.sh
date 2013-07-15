0eb19a1860f8d5ab00fb11f46f1db1d061de87a2
bis5 <bis5@bis5.mydns.jp>
2013-07-15 21:39:21 +0900

    create symlinks script

---
+++ create_symlinks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+LIST='.vimrc .vim .bash_aliases'
+if [ "$PWD" == "" ] ; then PWD=`pwd` ; fi
+for f in $LIST ; do
+	if [ -e ~/$f ] ; then
+		mv ~/$f ~/$f.bak
+	fi
+	ln -s $PWD/$f ~/
+done
